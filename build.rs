b11e5bdab40e764271d106de09363dea43fc9f1f
James Aprosail <aprosail@outlook.com>
2025-04-27 21:13:41 +0800

    feat!: migrate build child repo

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 // Copyright (c) 2025-present All contributors of this project
 // All contributors are listed in CONTRIBUTORS.yaml at repository root.
 //
-// AutoStories is licensed under Mulan PSL v2.
+// WorkTrace is licensed under Mulan PSL v2.
 // You can use this software according to the terms
 // and conditions of the Mulan PSL v2.
 // You may obtain a copy of Mulan PSL v2 at:
@@ -20,25 +20,19 @@
 use std::{
     env::var,
     fs::{copy, read_to_string},
-    path::PathBuf,
+    path::{Path, PathBuf},
+    slice::Iter,
 };
-use worktrace_generator::{fmt::eol, license::LicenseNotationGenerator};
+use worktrace_build::{fmt::eol, license::LicenseNotationGenerator};
 
 fn main() -> std::io::Result<()> {
     let root = PathBuf::from(var("CARGO_MANIFEST_DIR").unwrap());
-    update_cargo_license(&root).ok();
-    update_child_repo_license_files(&root);
+    update_cargo_license(&root)?;
+    update_assets(&root)?;
     Ok(())
 }
 
-const GENERATOR_CHILD_REPO_PATH: &str = "gen";
-
-fn update_child_repo_license_files(root: &PathBuf) {
-    let generator = root.join(GENERATOR_CHILD_REPO_PATH);
-    ["LICENSE", "CONTRIBUTORS.yaml"].iter().for_each(|name| {
-        copy(root.join(name), generator.join(name)).ok();
-    });
-}
+const WORKTRACE_BUILD: &str = "worktrace-build";
 
 fn update_cargo_license(root: &PathBuf) -> std::io::Result<()> {
     let generator = LicenseNotationGenerator {
@@ -47,7 +41,23 @@ fn update_cargo_license(root: &PathBuf) -> std::io::Result<()> {
         separator: "=== Auto generated, DO NOT EDIT ABOVE ===".into(),
         eol: eol::LF,
     };
-    generator.update_dir(&root.join(GENERATOR_CHILD_REPO_PATH).join("src"))?;
+    generator.update_dir(&root.join(WORKTRACE_BUILD).join("src"))?;
     generator.update_dir(&root.join("src"))?;
     generator.update_file(&root.join("build.rs"))
+}
+
+fn update_assets(root: &PathBuf) -> std::io::Result<()> {
+    let files = ["LICENSE", "CONTRIBUTORS.yaml"].iter();
+    copy_assets(&files, &root, &root.join(WORKTRACE_BUILD))
+}
+
+fn copy_assets(
+    files: &Iter<impl AsRef<Path>>,
+    src: &PathBuf,
+    out: &PathBuf,
+) -> std::io::Result<()> {
+    for name in files.as_slice() {
+        copy(src.join(name), out.join(name))?;
+    }
+    Ok(())
 }

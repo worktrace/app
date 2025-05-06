241ae9fb4f0a438cf717e3e716bfca7f23ce0e79
James Aprosail <aprosail@outlook.com>
2025-05-06 22:58:41 +0800

    feat: terminal colors

--- src/utils/terminal/color.rs
+++ src/utils/terminal/color.rs
@@ -77,12 +77,12 @@ pub trait TerminalColorUsages {
     }
 }
 
-pub struct TerminalColor<'a> {
+pub struct BasicTerminalColor<'a> {
     pub foreground: &'a str,
     pub background: &'a str,
 }
 
-impl TerminalColorUsages for TerminalColor<'_> {
+impl TerminalColorUsages for BasicTerminalColor<'_> {
     #[inline]
     fn foreground(&self) -> impl Display {
         self.foreground
@@ -94,12 +94,108 @@ impl TerminalColorUsages for TerminalColor<'_> {
     }
 }
 
-pub struct CustomTerminalColor {
+pub const BLACK: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[30m",
+    background: "\x1b[40m",
+};
+
+pub const RED: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[31m",
+    background: "\x1b[41m",
+};
+
+pub const GREEN: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[32m",
+    background: "\x1b[42m",
+};
+
+pub const YELLOW: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[33m",
+    background: "\x1b[43m",
+};
+
+pub const BLUE: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[34m",
+    background: "\x1b[44m",
+};
+
+pub const MAGENTA: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[35m",
+    background: "\x1b[45m",
+};
+
+pub const CYAN: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[36m",
+    background: "\x1b[46m",
+};
+
+pub const WHITE: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[37m",
+    background: "\x1b[47m",
+};
+
+pub const HI_BLACK: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[90m",
+    background: "\x1b[100m",
+};
+
+pub const HI_RED: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[91m",
+    background: "\x1b[101m",
+};
+
+pub const HI_GREEN: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[92m",
+    background: "\x1b[102m",
+};
+
+pub const HI_YELLOW: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[93m",
+    background: "\x1b[103m",
+};
+
+pub const HI_BLUE: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[94m",
+    background: "\x1b[104m",
+};
+
+pub const HI_MAGENTA: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[95m",
+    background: "\x1b[105m",
+};
+
+pub const HI_CYAN: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[96m",
+    background: "\x1b[106m",
+};
+
+pub const HI_WHITE: BasicTerminalColor = BasicTerminalColor {
+    foreground: "\x1b[97m",
+    background: "\x1b[107m",
+};
+
+pub struct TerminalColor {
     pub foreground: String,
     pub background: String,
 }
 
-impl TerminalColorUsages for CustomTerminalColor {
+impl TerminalColor {
+    pub fn from_code(code: u8) -> Self {
+        Self {
+            foreground: format!("\x1b[38;5;{}m", code),
+            background: format!("\x1b[48;5;{}m", code),
+        }
+    }
+
+    pub fn from_rgb(red: u8, green: u8, blue: u8) -> Self {
+        Self {
+            foreground: format!("\x1b[38;2;{};{};{}m", red, green, blue),
+            background: format!("\x1b[48;2;{};{};{}m", red, green, blue),
+        }
+    }
+}
+
+impl TerminalColorUsages for TerminalColor {
     #[inline]
     fn foreground(&self) -> impl Display {
         &self.foreground
